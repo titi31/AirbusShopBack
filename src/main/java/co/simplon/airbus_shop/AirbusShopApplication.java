f29d0540cf4dcbb089277a7c25c1a518269180c5
timothe laude <timothelaude@MacBook-Pro-de-timothe.local>
2019-09-23 14:48:15 +0200

    first commit

---
+++ src/main/java/co/simplon/airbus_shop/AirbusShopApplication.java
@@ -0,0 +1,13 @@
+package co.simplon.airbus_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbusShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirbusShopApplication.class, args);
+    }
+
+}
