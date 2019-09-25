bd9d6ab426516142496d0890adad26dd9b6869ef
timothe laude <timothelaude@MacBook-Pro-de-timothe.local>
2019-09-25 09:25:41 +0200

    add table category

--- src/main/java/co/simplon/airbus_shop/AirbusShopApplication.java
+++ src/main/java/co/simplon/airbus_shop/AirbusShopApplication.java
@@ -1,9 +1,12 @@
 package co.simplon.airbus_shop;
 
+import co.simplon.airbus_shop.dao.CategoryRepository;
 import co.simplon.airbus_shop.dao.ProductRepository;
 import co.simplon.airbus_shop.dao.UserRepository;
-import co.simplon.airbus_shop.entities.Products;
+import co.simplon.airbus_shop.entities.Category;
+import co.simplon.airbus_shop.entities.Product;
 import co.simplon.airbus_shop.entities.User;
+import net.bytebuddy.utility.RandomString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,8 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 
 
 @SpringBootApplication
@@ -24,6 +28,8 @@ public class AirbusShopApplication implements CommandLineRunner  {
     @Autowired
     private UserRepository userRepository;
     @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
     private RepositoryRestConfiguration restConfigurationUser;
     public static void main(String[] args) {
         SpringApplication.run(AirbusShopApplication.class, args);
@@ -31,12 +37,24 @@ public class AirbusShopApplication implements CommandLineRunner  {
 
     @Override
     public void run(String... args) throws Exception {
-        restConfiguration.exposeIdsFor(Products.class);
-        productRepository.save(new Products(null,"macboook pro",1200,5));
-        productRepository.save(new Products(null,"bracelets",300,3));
-        productRepository.save(new Products(null,"parfum",40,6));
-        productRepository.findAll().forEach(p->{
-            System.out.println(p.toString());
+        restConfiguration.exposeIdsFor(Product.class);
+       //categoryRepository.save(new Category(null,"computer",null,null ,null));
+       categoryRepository.save( new Category(null,"jewelry",null,null,null ));
+       categoryRepository.save( new Category(null,"perfumery",null,null,null ));
+       categoryRepository.save(new Category(null,"computer",null,null,null ));
+       Random rnd= new Random();
+        categoryRepository.findAll().forEach(c -> {
+            for(int i=0;i<10;i++) {
+                Product p = new Product();
+                p.setName(RandomString.make(18));
+                p.setCurrentprice(100 + rnd.nextInt(10000));
+                p.setAvailable(rnd.nextBoolean());
+                p.setPromotion(rnd.nextBoolean());
+                p.setSelected(rnd.nextBoolean());
+                p.setCategory(c);
+                p.setPhotoName("unknown.png");
+                productRepository.save(p);
+            }
         });
         restConfigurationUser.exposeIdsFor(User.class);
         userRepository.save(new User(null,"admin","azerty","admin"));
